27c55d8d2c847e700fea204be39632e947e4981d
Lewis Hammond <s1800707@connect.glos.ac.uk>
2022-12-04 13:12:31 +0000

    part 1

--- AdventOfCode/Day4/Day4.cpp
+++ AdventOfCode/Day4/Day4.cpp
@@ -1,20 +1,38 @@
 // Day4.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include <string>
+#include <fstream>
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::string lineString;
+    std::ifstream inFile;
+
+    inFile.open("input.txt");
+    if (!inFile.is_open())
+    {
+        std::cout << "Failed to open file" << std::endl;
+        return -1;
+    }
+
+    int Fullcount = 0;
+    while(std::getline(inFile, lineString))
+    {
+        int l1 = -1; int l2 = -1; int r1 = -1; int r2 = -1;
+
+        sscanf_s(lineString.c_str(), "%i-%i,%i-%i", &l1, &l2, &r1, &r2);
+
+        //Full Overlap
+        if((l1 >= r1 && l2 <= r2) || (r1 >= l1 && r2 <= l2))
+        {
+            Fullcount++;
+        }
+
+        //Partial Overlap
+    }
+
+    std::cout << Fullcount << std::endl;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
