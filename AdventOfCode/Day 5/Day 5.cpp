d8bc3337077dca909075ffa0bad028613f104307
Lewis Hammond <55703082+LewisHammond-uog@users.noreply.github.com>
2022-12-06 15:11:34 +0000

    Squashed commit of the following:
    commit 86c8a37db40944a7dff94ee21341842de3150c11
    Author: Lewis Hammond <55703082+LewisHammond-uog@users.noreply.github.com>
    Date:   Tue Dec 6 15:10:06 2022 +0000
    
        Part 2
    
    commit 01646b389dd115458541106c9fff948d7f54805f
    Author: Lewis Hammond <55703082+LewisHammond-uog@users.noreply.github.com>
    Date:   Tue Dec 6 14:56:17 2022 +0000
    
        Part 1 Complete
    
    commit 01c5f717fa5868f9e134c262f54f944b65394cd8
    Author: Lewis Hammond <55703082+LewisHammond-uog@users.noreply.github.com>
    Date:   Tue Dec 6 13:46:42 2022 +0000
    
        Create Project

---
+++ AdventOfCode/Day 5/Day 5.cpp
@@ -0,0 +1,93 @@
+// Day 5.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <stack>
+
+#define LETTER_POS 1
+#define BLOCK_LEN 3
+#define BLOCK_LEN_WITH_SPACE BLOCK_LEN + 1
+#define BLOCK_STACK_COUNT 9
+
+
+int main()
+{
+    std::string lineString;
+    std::ifstream inFileStacks;
+    std::ifstream inFileInstructions;
+
+    inFileStacks.open("input_blocks.txt");
+    inFileInstructions.open("input_instructions.txt");
+
+    if (!inFileStacks.is_open() << !inFileInstructions.is_open())
+    {
+        std::cout << "Failed to open file" << std::endl;
+        return -1;
+    }
+
+    std::stack < std::string > stacks[BLOCK_STACK_COUNT];
+
+
+    //Read stacks
+    while (getline(inFileStacks, lineString)) 
+    {
+        int currentStack = 0;
+        for (int i = 0; i < lineString.length(); i += BLOCK_LEN_WITH_SPACE)
+        {
+            if (lineString[i] == ' ') {
+                ++currentStack;
+                continue;
+            }
+            
+            std::string item = std::string(1, lineString[i + LETTER_POS]);
+
+            stacks[currentStack].push(item);
+            ++currentStack;
+        }
+    }
+    inFileStacks.close();
+
+    //Read and execute instructions
+  
+    while (getline(inFileInstructions, lineString))
+    {
+        int numToMove = 0;
+        int stackFrom = 0;
+        int stackTo = 0;
+
+        sscanf_s(lineString.c_str(), "move %i from %i to %i", &numToMove, &stackFrom, &stackTo);
+
+        //Move from old stack
+        std::stack<std::string> tempMove;
+        for (int i = 0; i < numToMove; ++i) 
+        {
+            //-1 because instructions are 1 indexed, stacks are 0 indexed
+            std::string moveChar = stacks[stackFrom - 1].top();
+            stacks[stackFrom - 1].pop();
+
+            tempMove.push(moveChar);
+        }
+
+        //Put on new stack
+        for (int i = 0; i < numToMove; ++i)
+        {
+            std::string moveChar = tempMove.top();
+            tempMove.pop();
+                
+            stacks[stackTo - 1].push(moveChar);
+        }
+    }
+    inFileInstructions.close();
+
+    //List the tops of each stack
+    for (int i = 0; i < BLOCK_STACK_COUNT; ++i) 
+    {
+        std::cout << stacks[i].top();
+    }
+}
+
