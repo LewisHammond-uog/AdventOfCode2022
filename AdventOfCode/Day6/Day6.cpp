e6f5ef835c8716e9bffd82bc947b246fdbb82d9e
Lewis Hammond <55703082+LewisHammond-uog@users.noreply.github.com>
2022-12-09 14:32:55 +0000

    Squashed commit of the following:
    commit aebde5ea0e598150b76d34c08e7b29edc936aedf
    Author: Lewis Hammond <55703082+LewisHammond-uog@users.noreply.github.com>
    Date:   Fri Dec 9 14:32:34 2022 +0000
    
        Part 2
    
    commit 03756ee55cb16fad8676e8c7523b0c6a81540ad9
    Author: Lewis Hammond <55703082+LewisHammond-uog@users.noreply.github.com>
    Date:   Fri Dec 9 14:31:35 2022 +0000
    
        Part 1
    
    commit bbca4b16caef6c91f19969761f1090414dc46969
    Author: Lewis Hammond <55703082+LewisHammond-uog@users.noreply.github.com>
    Date:   Tue Dec 6 15:13:57 2022 +0000
    
        Create New Project

---
+++ AdventOfCode/Day6/Day6.cpp
@@ -0,0 +1,59 @@
+// Day6.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <queue>
+
+#define MARKER_LOCATION 14
+
+int main()
+{
+    std::string lineString;
+    std::ifstream inFile;
+
+    inFile.open("input.txt");
+    if (!inFile.is_open())
+    {
+        std::cout << "Failed to open file" << std::endl;
+        return -1;
+    }
+
+    int count = 0;
+    char a;
+    std::deque<char> charQueue;
+    while (inFile >> a)
+    {
+        ++count;
+
+        charQueue.push_back(a);
+
+        //Check if the queue contains the current char, if it does not then we have a string of 4 unique characters
+        bool hasDupes = false;
+        if (charQueue.size() >= MARKER_LOCATION) {
+            //Check there are no repeat characters in the queue
+            for (int i = 0; i < charQueue.size(); ++i) 
+            {
+                if (std::count(charQueue.begin(), charQueue.end(), charQueue[i]) >= 2) 
+                {
+                    hasDupes = true;
+                    break;
+                }
+            }
+        }
+
+        if (!hasDupes && charQueue.size() >= MARKER_LOCATION)
+        {
+            std::cout << count;
+            break;
+        }
+
+        if (charQueue.size() >= MARKER_LOCATION) {
+            charQueue.pop_front();
+        }
+    }
+
+
+}
