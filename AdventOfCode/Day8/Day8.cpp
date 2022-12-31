5f7ff14b13704eac3e1ce976d6ca2f7d946fab33
Lewis Hammond <bubblemapgaming@gmail.com>
2022-12-31 19:19:26 +0000

    Profile Time
    181 to beat

--- AdventOfCode/Day8/Day8.cpp
+++ AdventOfCode/Day8/Day8.cpp
@@ -26,7 +26,14 @@ int main()
 
     TreeGrid grid = TreeGrid(fileRead);
 
+    using milli = std::chrono::milliseconds;
+    auto start = std::chrono::high_resolution_clock::now();
     std::cout << grid.GetBestScore();
+    auto finish = std::chrono::high_resolution_clock::now();
+    std::cout << "myFunction() took "
+        << std::chrono::duration_cast<milli>(finish - start).count()
+        << " milliseconds\n";
+
 
 }
 
